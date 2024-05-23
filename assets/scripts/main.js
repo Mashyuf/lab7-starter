b1c8f7fc277661472182e19f6541352be2408933
Matthew Fang <64571966+Mashyuf@users.noreply.github.com>
2024-05-22 18:57:58 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -100,7 +100,6 @@ function initFormHandler() {
 		// B9. TODO - Get the recipes array from localStorage, add this new recipe to it, and
 		//            then save the recipes array back to localStorage
 		let recipeList = getRecipesFromStorage();
-		recipeObject = Object.assign({}, recipeObject)
 		recipeList.push(recipeObject);
 		saveRecipesToStorage(recipeList);
 	});
